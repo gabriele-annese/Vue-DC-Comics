0ba21ea4f63ad44a9aa55101f01dfbf9455c0e3d
gabriele-annese <gannese06@gmail.com>
2021-11-24 18:06:23 +0100

    add cursor-pointer img

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -31,6 +31,7 @@ export default {
                     width: 100%;
                     height: 100%;
                     object-fit: cover;
+                     cursor: pointer;
                 }
             }
 </style>
